3c150265d8dff2584578c78d1c554ed3340be5a0
Andrés Torres <125112555+andres-stg@users.noreply.github.com>
2024-02-29 02:38:09 +0000

    Adelantada la logica y los aspectos visuales del cards generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,37 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  function generadorAleatorioPoker() {
+    let cartaTipos = ["♥", "♦", "♠", "♦"];
+
+    let cartaAleatorios =
+      cartaTipos[Math.floor(Math.random() * cartaTipos.length)];
+
+    document.getElementById("cartaTop").innerHTML = cartaAleatorios;
+    document.getElementById("cartaBottom").innerHTML = cartaAleatorios;
+
+    let carta = document.getElementById("carta");
+    carta.classList.add(cartaAleatorios);
+
+    let numeroCarta = [
+      "A",
+      "J",
+      "K",
+      "Q",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10"
+    ];
+
+    document.getElementById("numero").textContent =
+      numeroCarta[Math.floor(Math.random() * numeroCarta.length)];
+  }
+
+  generadorAleatorioPoker();
 };
